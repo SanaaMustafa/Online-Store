f757f25e392c2931da4a7127d21462b73702372f
sanaamustafa <sanaamostafa59@gmail.com>
2016-08-28 12:25:09 -0700

    Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,12 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('country');
+            $table->string('city');
+            $table->string('state');
+            $table->integer('zip');
+            $table->integer('phone');
+            $table->string('Address');
             $table->rememberToken();
             $table->timestamps();
         });
