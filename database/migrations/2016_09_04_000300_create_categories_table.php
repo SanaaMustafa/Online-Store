0cd71ace708bd9e4f2a2ebaa820777e0c2fd93d4
sanaamustafa <sanaamostafa59@gmail.com>
2016-09-03 17:13:24 -0700

    Categories Migration

---
+++ database/migrations/2016_09_04_000300_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('categories', function (Blueprint $table) {
+            $table->integer('id')->unsigned();
+            $table->foreign('id')->references('category')->on('products');
+          $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('table',  function (Blueprint $table) {
+            $table->dropForeign(['categories_id_foreign']);
+        });
+
+        Schema::drop('categories');
+    }
+}
