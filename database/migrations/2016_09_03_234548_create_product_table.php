f033f5994ec77f35cd1e8a709a6c77fa5bf5a2f6
sanaamustafa <sanaamostafa59@gmail.com>
2016-09-03 17:01:15 -0700

    Product Migrations&Model

---
+++ database/migrations/2016_09_03_234548_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->float('price');
+            $table->string('shortdes');
+            $table->text('longdes');
+            $table->string('image');
+            $table->integer('category');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('products');
+    }
+}
